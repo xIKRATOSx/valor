0818af60a7073eddcf37a8e02ee26b225ec2657c
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2022-07-25 15:09:22 +0530

    Add files via upload

---
+++ plugins/youtube-yts.js
@@ -0,0 +1,27 @@
+import { youtubeSearch } from '@bochilteam/scraper'
+let handler = async (m, { text, command, usedPrefix }) => {
+  if (!text) throw `Type "${usedPrefix}${command} <name>" to search audio/video.\n\nFor Example:\n${usedPrefix}${command} Bolenath Ji`
+  const { video, channel } = await youtubeSearch(text)
+  let teks = [...video, ...channel].map(v => {
+    switch (v.type) {
+      case 'video': return `
+🔖 ᴛɪᴛʟᴇ: *${v.title}*
+🔗 ᴜʀʟ: ${v.url}
+⌛ ᴅᴜʀᴀᴛɪᴏɴ: ${v.durationH}
+📡 ᴩᴜʙʟɪsʜᴇᴅ: ${v.publishedTime}
+👀️ ᴠɪᴇᴡs: ${v.view}
+      `.trim()
+      case 'channel': return `
+📌 *${v.channelName}* (${v.url})
+🧑‍🤝‍🧑 _${v.subscriberH} (${v.subscriber}) Subscriber_
+🎥 ${v.videoCount} video
+`.trim()
+    }
+  }).filter(v => v).join('\n\n========================\n\n')
+  m.reply(teks)
+}
+handler.help = ['yts'].map(v => v + ' <name>')
+handler.tags = ['TOOLS']
+handler.command = /^(yts|ytsearch)$/i
+
+export default handler
